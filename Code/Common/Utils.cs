c9f8836ae4b3a7af756d66b7a4d8a9cbef34232c
Farkhat Ovezklychev <f.ovezklychev@rrs.catering>
2024-09-29 22:45:07 +0300

    fix: code organized and cleaned up

---
+++ Code/Common/Utils.cs
@@ -0,0 +1,34 @@
+﻿using Godot;
+
+namespace galacticinvasion.Code.Common;
+
+public static class Utils
+{
+    public static (Vector2 minBounds, Vector2 maxBounds) CalculateScreenBounds(this Camera3D camera, Vector2 viewportSize)
+    {
+        Vector3 topLeft = ProjectScreenPositionToWorld(camera, new Vector2(0, 0));
+        Vector3 topRight = ProjectScreenPositionToWorld(camera, new Vector2(viewportSize.X, 0));
+        Vector3 bottomLeft = ProjectScreenPositionToWorld(camera, new Vector2(0, viewportSize.Y));
+        Vector3 bottomRight = ProjectScreenPositionToWorld(camera, new Vector2(viewportSize.X, viewportSize.Y));
+
+        // Вычисляем минимальные и максимальные значения по X и Z
+        return (new Vector2(
+            Mathf.Min(Mathf.Min(topLeft.X, bottomLeft.X), Mathf.Min(topRight.X, bottomRight.X)),
+            Mathf.Min(Mathf.Min(topLeft.Z, bottomLeft.Z), Mathf.Min(topRight.Z, bottomRight.Z))
+        ),
+        new Vector2(
+            Mathf.Max(Mathf.Max(topLeft.X, bottomLeft.X), Mathf.Max(topRight.X, bottomRight.X)),
+            Mathf.Max(Mathf.Max(topLeft.Z, bottomLeft.Z), Mathf.Max(topRight.Z, bottomRight.Z))
+        ));
+    }
+
+    public static Vector3 ProjectScreenPositionToWorld(this Camera3D camera, Vector2 screenPosition)
+    {
+        Vector3 from = camera.ProjectRayOrigin(screenPosition);
+        Vector3 direction = camera.ProjectRayNormal(screenPosition);
+
+        // Рассчитываем пересечение луча с плоскостью Y = planeY (planeY убран, предполагается, что плоскость на нуле)
+        float t = /* planeY */ - from.Y / direction.Y;
+        return from + direction * t;
+    }
+}
---
+++ Code/RocketControl/Rocket.cs
@@ -0,0 +1,73 @@
+using galacticinvasion.Code.Common;
+using Godot;
+
+namespace galacticinvasion.Code.RocketControl;
+
+public partial class Rocket : CharacterBody3D
+{
+    [Export]
+    private float Smoothness = 5f;  // Коэффициент плавности изменения скорости
+    [Export]
+    private float _xBoundaryOffset = 7f;
+    [Export]
+    private float _zBoundaryOffset = 12f;
+    [Export]
+    private Camera3D _camera;  // Ссылка на камеру
+
+    private Vector2 _inputPosition;
+    private Vector3 _targetPosition;
+
+    private Vector2 _minBounds;  // Минимальные границы по X и Z
+    private Vector2 _maxBounds;  // Максимальные границы по X и Z
+
+    public override void _Ready()
+    {
+        // Инициализируем начальную целевую позицию ракетой
+        _targetPosition = Position;
+
+        // Вычисляем границы экрана в мировых координатах
+        (_minBounds, _maxBounds) = _camera.CalculateScreenBounds(GetViewport().GetVisibleRect().Size);
+    }
+
+    // Обработка ввода для сенсорных экранов
+    public override void _Input(InputEvent @event)
+    {
+        // Если сенсорное касание экрана (первое нажатие)
+        if (@event is InputEventScreenTouch touchEvent && touchEvent.Pressed)
+        {
+            // Обновляем целевую позицию для сенсорного нажатия
+            _inputPosition = touchEvent.Position;
+        }
+        // Если пользователь проводит пальцем по экрану
+        else if (@event is InputEventScreenDrag dragEvent)
+        {
+            // Обновляем целевую позицию на основе перемещения пальца
+            _inputPosition = dragEvent.Position;
+        }
+        else if (@event is InputEventMouse mouseEvent)
+        {
+            _inputPosition = mouseEvent.Position;
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        // Управление ракетой
+
+        // Обновляем целевую позицию на основе ввода (транслируем позицию на экране в позицию в мире)
+        _targetPosition = _camera.ProjectScreenPositionToWorld(_inputPosition);
+
+        // Ограничиваем позицию ракеты границами камеры (посчитаны исключительно для камеры)
+        LimitPositionByBoundaries();
+
+        // Плавное движение ракеты с помощью Lerp
+        Position = Position.Lerp(_targetPosition, Smoothness * (float)delta);
+    }
+
+    public void LimitPositionByBoundaries()
+    {
+        _targetPosition.X = Mathf.Clamp(_targetPosition.X, _minBounds.X + _xBoundaryOffset, _maxBounds.X - _xBoundaryOffset);
+        _targetPosition.Z = Mathf.Clamp(_targetPosition.Z, _minBounds.Y + _zBoundaryOffset, _maxBounds.Y - _zBoundaryOffset);
+    }
+
+}
